d6318ea03a9e47d8bff4c6b3d908cc333897ff25
Adit Malhotra <aditmalhotra8@gmail.com>
2023-09-05 22:26:59 +0530

    generator

--- generator/gen.cpp
+++ generator/gen.cpp
@@ -1,77 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int maxVal=20;
-
-void generateRandomArray(int* array,int l)
-{
-    for(int x=0;x<l;x++)
-    {
-        int temp_num=(rand()%maxVal);
-        array[x]=temp_num+1;
-    }
-}
-
-void writeToFile(int* array,int n, ofstream& file)
-{
-    for(int x=0;x<n;x++)
-    {
-        file<<array[x]<<" ";
-    }
-    file<<'\n';
-}
+using namespace std::chrono;
+mt19937 gen(steady_clock::now().time_since_epoch().count()); 
 
 int main()
 {
     srand(time(0));
-    int z,l,time;
+    int z,l,time,maxval;
     cout<<"enter z:";
     cin>>z;
     cout<<"enter l:";
     cin>>l;
     cout<<"enter time:";
     cin>>time;
-    cout<<"enter output file name (leave empty for default->in.txt):";
-    string outputfilename = "./../io_files/in.txt";
-    string x;cin>>x;
-    if(x!="") outputfilename = x;
-
-    ofstream outputFile(outputfilename);
-    if (!outputFile.is_open()) {
-        cerr << "Failed to open the file for writing." << std::endl;
-        exit(0);
-    }
-    outputFile<<time<<'\n';
-    outputFile<<z<<'\n';
-    outputFile<<l<<'\n';
-
+    cout<<"enter maxval:";
+    cin>>maxval;
+    uniform_int_distribution<int> rnd(1,maxval);
+    string outputfilename = "./io_files/in_"+to_string(z)+"z_"+to_string(l)+"l.txt";
+    ofstream outFile(outputfilename);
+    outFile<<time<<endl;
+    outFile<<z<<endl;
+    outFile<<l<<endl;
+    auto generateRandomArray= [&](int* array,int l,int idx)
+    {
+        for(int x=0;x<l;x++)
+        {
+            if(x!=idx)
+            array[x]=rnd(gen);
+            else array[x]=0;
+        }
+    };
     int array1[z];
     for(int i=0;i<z;i++)
     {
-        generateRandomArray(array1,z);
-        writeToFile(array1,z,outputFile);
+        generateRandomArray(array1,z,i);
+        for(auto x:array1) outFile<<x<<" ";
+        outFile<<endl;
     }
-    vector<int> array2(l);
-    vector<vector<int>> matrix(l);
+    int array2[l][l];
     for(int i=0;i<l;i++)
     {
-        generateRandomArray(&array2[0],l);
-        matrix[i]=array2;
-    }
-
-    for(int i=0;i<l;i++)
-    {
-        matrix[i][i]=0;
-        for(int j=0;j<i;j++)
-        {
-            matrix[j][i]=matrix[i][j];
+        for(int j=i;j<l;j++){
+            if(i==j) array2[i][j]=0;
+            else{
+                array2[i][j]=rnd(gen);
+                array2[j][i]=array2[i][j];
+            }
         }
     }
-
-    for(int i=0;i<l;i++)
-    {
-        writeToFile(&matrix[i][0],l,outputFile);
+    for(int i=0;i<l;i++){
+        for(int j=0;j<l;j++) outFile<<array2[i][j]<<" ";
+        outFile<<endl;
     }
-    outputFile.close();
+    outFile.close();
     return 0;
 }
